fee59dc32d30e57d88cbbf80f768d6abd9d4ba60
zihasyu <1045839067@qq.com>
2025-12-09 16:14:49 +0800

    update kadane

--- 209.长度最小的子数组.cpp
+++ 209.长度最小的子数组.cpp
@@ -11,6 +11,22 @@ class Solution
 public:
     int minSubArrayLen(int target, vector<int> &nums)
     {
+        int left = 0, right = 0, sum = 0, minLen = INT_MAX;
+        while (right < nums.size() || sum >= target)
+        {
+            if (sum < target)
+            {
+                sum += nums[right];
+                right++;
+            }
+            else if (sum >= target)
+            {
+                minLen = min(minLen, right - left);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return minLen == INT_MAX ? 0 : minLen;
     }
 };
 // @lc code=end
