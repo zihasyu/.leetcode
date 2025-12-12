0b9f1778305f5d4d28a66b1348975aa9fc147347
zihasyu <1045839067@qq.com>
2025-12-12 20:36:55 +0800

    adjust tree

--- 560.和为-k-的子数组.cpp
+++ 560.和为-k-的子数组.cpp
@@ -3,13 +3,22 @@
  *
  * [560] 和为 K 的子数组
  */
-
+#include <bits/stdc++.h>
+using namespace std;
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        
+    int subarraySum(vector<int> &nums, int k)
+    {
+        int ans = 0;
+        if (nums.empty())
+            return ans;
+        int sum = 0, left = 0, right = 0;
+        while (right < nums.size())
+        {
+            if (sum < k)
+        }
     }
 };
 // @lc code=end
-
