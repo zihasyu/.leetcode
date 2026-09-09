fb3679338ad7ff00aa9c0dee0a47aba04f646e75
zihasyu <1045839067@qq.com>
2026-09-09 15:13:37 +0800

    update

---
+++ 1.两数之和.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=cpp
+ *
+ * [1] 两数之和
+ */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> Map;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (Map.find(nums[i]) != Map.end())
+            {
+                return {i, Map[nums[i]]};
+            }
+            else
+            {
+                Map.insert({target - nums[i], i});
+            }
+        }
+        return {};
+    }
+};
+// @lc code=end
